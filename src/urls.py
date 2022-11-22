d1d79ef9a3da09f4fa5624d6e5f24cee4e2fc1df
Stuart Nam <tokarinrika@gmail.com>
2022-11-22 15:01:47 +0700

    Update HTML render files

--- src/urls.py
+++ src/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from src import views
 
-urlpatterns = [path("", views.home, name = "home")]
+urlpatterns = [path("", views.home, name = "home"), path("src/<name>", views.index, name = "index")]
