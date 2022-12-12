34aec5c3f2145ab6c60c52ad026c3ac51307fdca
HarryHaha1010 <tung.huynhthanhkhmt@hcmut.edu.vn>
2022-12-13 03:52:16 +0700

    get the route from pre-defined MCPs

--- API/views.py
+++ API/views.py
@@ -11,29 +11,29 @@ def route(request):
     {
       "vehicle_id": "my_vehicle",
       "start_address": {
-        "location_id": "berlin",
-        "lon": 13.406,
-        "lat": 52.537
+        "location_id": "MCP2",
+        "lat": 10.7716128,
+        "lon": 106.6592997
       }
     }
-  ],
+    ],
   "services": [
     {
-      "id": "hamburg",
-      "name": "visit_hamburg",
+      "id": "Cơ_sở_2",
+      "name": "Cơ_sở_2",
       "address": {
-        "location_id": "hamburg",
-        "lon": 9.999,
-        "lat": 53.552
+        "location_id": "MCP1",
+        "lat": 10.8799198, 
+        "lon": 106.8047006
       }
     },
     {
-      "id": "munich",
-      "name": "visit_munich",
+      "id": "Quận_11",
+      "name": "Nhân_viên_Sơn",
       "address": {
-        "location_id": "munich",
-        "lon": 11.57,
-        "lat": 48.145
+        "location_id": "MCP3",
+        "lat": 10.7711526, 
+        "lon": 106.6529604
       }
     }
     ]
@@ -42,7 +42,13 @@ def route(request):
   'Content-Type': 'application/json'
     }   
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)#response in json format
+    route=(response.json()) #dict format
+    mes=""
+    for i in route['solution']['routes'][0]['activities']: #get the order of visting node
+        mes+=' -> '+i['location_id'] #display order of MCP
+    mes=mes[4:]
+    print(mes)
 
     return JsonResponse(response.json())
 # Create your views here.
