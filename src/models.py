fae180dd6c2bf64c8714a8a44d54de3d6d9837bb
sonkate <son.trankaty@hcmut.edu.vn>
2022-12-13 11:07:49 +0700

    route

--- src/models.py
+++ src/models.py
@@ -55,6 +55,6 @@ class Mission(models.Model):
     description = models.CharField(max_length=200, blank=False)
     MCP = models.ManyToManyField(MCP, blank= True, null= True)
     vehicle = models.ForeignKey(Vehicle,on_delete=models.PROTECT, blank= False)
-    route = models.CharField(max_length=200)
+    route = models.CharField(max_length=200, blank= True)
     def MCPs(self):
         return ', '.join([str(a.name) for a in self.MCP.all()])
